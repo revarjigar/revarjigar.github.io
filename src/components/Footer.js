5c16d72ed308880bbb29eeeb440d29bb5c5fd358
Jigar Revar <jigarrevar@macbooky-macbookface.local>
2019-04-05 12:29:08 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,18 @@ class Footer extends Component {
       <div className="footer-container">
         <div className="footer">
           <div className="phone-left"/>
-          <h1 className="font-medium footer-header">TEST TEST TEST!</h1>
-          <p className="footer-sub-header">TEST TEST TEST</p>
+          <h1 className="font-medium footer-header">Get in touch with me now!</h1>
+          <p className="footer-sub-header">Jigar Revar</p>
           <div className="download-cta-footer">
             <DownloadCTA/>
           </div>
           <div className="footer-contact">
-            <p>TEST TEST TEST</p>
-            <p className="footer-contact2">TEST TEST TEST </p>
-            <a className="footer-contact2" href="mailto:TEST TEST TEST">TEST TEST TEST</a>
-            <ul><li><a href="/terms-of-use">TEST TEST TEST</a></li><li><a href="/privacy-policy">TEST TEST TEST</a></li></ul>
+            <p>MUMBAI, India</p>
+            <p className="footer-contact2">Phone: +91 0000000000</p> <a className="footer-contact2" href="mailto:revarjigar@gmail.com">Click here to MAIL me</a>
+            <ul>
+              <li><a href="https://www.linkedin.com/in/jigar-revar-993a28126/" target="_blank">LINKEDIN</a></li>
+              <li><a href="https://github.com/revarjigar/" target="_blank">GITHUB</a></li>
+            </ul>
           </div>
           <div className="phone-right"/>
         </div>
